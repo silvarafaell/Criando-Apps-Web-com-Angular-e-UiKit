a5384cd55f148f55d0f56e9b32c25836493000fe
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-15 15:27:06 -0300

    Tela de Login

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPassordPageComponent } from './reset-passord-page/reset-passord-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPassordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ petshop/src/app/reset-passord-page/reset-passord-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-passord-page',
+  templateUrl: './reset-passord-page.component.html',
+  styleUrls: ['./reset-passord-page.component.css']
+})
+export class ResetPassordPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

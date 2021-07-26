9cdbc043b0d3a532f0a88e6a8dea4e7ea9c4685b
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-26 12:04:12 -0300

    Estilos Adicionais

--- petshop/src/app/pages/account/login-page/login-page.component.ts
+++ petshop/src/app/pages/account/login-page/login-page.component.ts
@@ -4,7 +4,8 @@ import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;

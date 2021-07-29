83becf3edf55fcc6962c3be4c07a3a90e65b2205
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-29 07:53:22 -0300

    Adicionando Loading

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 import { ProductCardComponent } from './components/store/product-card/product-card.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProductsPageComponent,
     CartPageComponent,
     FramePageComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
--- petshop/src/app/pages/account/login-page/login-page.component.ts
+++ petshop/src/app/pages/account/login-page/login-page.component.ts
@@ -9,6 +9,7 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class LoginPageComponent implements OnInit {
   public form: FormGroup;
+  public busy = false;
 
   constructor(
     private service: DataService,
@@ -31,31 +32,36 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
     const token = localStorage.getItem('petshop.token');
     if (token) {
-      console.log('Autenticando..');
+      this.busy = true;
       this
         .service
         .refreshToken()
         .subscribe(
           (data: any) => {
             localStorage.setItem('petshop.token', data.token);
+            this.busy = false;
           },
           (err) => {
             localStorage.clear();
+            this.busy = false;
           }
         );
     }
   }
 
   submit() {
+    this.busy = true;
     this
       .service
       .authenticate(this.form.value)
       .subscribe(
         (data: any) => {
           localStorage.setItem('petshop.token', data.token);
+          this.busy = false;
         },
         (err) => {
-          console.log(err)
+          console.log(err);
+          this.busy = false;
         }
       );
   }

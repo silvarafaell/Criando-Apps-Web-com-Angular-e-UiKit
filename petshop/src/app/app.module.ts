98fdbcd3e248ed75bd4b8a2a68710ed5378499cf
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-07-30 07:51:35 -0300

    Mask Directive

--- petshop/src/app/app.module.ts
+++ petshop/src/app/app.module.ts
@@ -14,9 +14,11 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
 import { ProductCardComponent } from './components/store/product-card/product-card.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+import { MaskDirective } from './directives/mask.directive';
 
 @NgModule({
   declarations: [
+    MaskDirective,
     AppComponent,
     NavbarComponent,
     LoginPageComponent,

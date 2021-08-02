cae4360ba53014129f34118ceb72d0cfc2aed284
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-08-02 13:48:52 -0300

    Logout

--- petshop/src/app/components/shared/navbar/navbar.component.ts
+++ petshop/src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { Security } from 'src/app/utils/security.util';
 
@@ -8,10 +9,15 @@ import { Security } from 'src/app/utils/security.util';
 })
 export class NavbarComponent implements OnInit {
   public user!: User;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.user = Security.getUser();
+  }
+
+  logout() {
+    Security.clear();
+    this.router.navigate(['/login']);
   }
 
 }

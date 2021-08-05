f0443dc6729c26bc28cc01f83d2f7b43918ae2ba
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-08-05 20:40:01 -0300

    Edição de Perfil: Parte 2

--- petshop/src/app/app-routing.module.ts
+++ petshop/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
 import { ResetPassordPageComponent } from './pages/account/reset-passord-page/reset-passord-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 import { FramePageComponent } from './pages/master/frame.page';
@@ -21,7 +22,9 @@ const routes: Routes = [
   {
     path: 'account',
     component: FramePageComponent,
+    canActivate: [AuthService],
     children: [
+      { path: '', component: ProfilePageComponent },
       { path: 'pets', component: PetsPageComponent },
     ]
   },
